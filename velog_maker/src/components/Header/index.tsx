476fae0dd6e18c04168ab837601d66d41aa280d3
전해림 <HAERIM060920@gmail.com>
2023-12-08 10:59:41 +0900

    feat : 바벨 플러그인 설정 #2

---
+++ velog_maker/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import * as S from "./style";
+
+const Header = () => {
+  return (
+    <S.HeaderContainer>
+      <S.Title>Velog_Maker</S.Title>
+      <div>
+        <S.Icon src="/githubicon.png" alt="githubicon" />
+      </div>
+    </S.HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ velog_maker/src/components/Header/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  height: 60px;
+  background-color: #4d7a96;
+`;
+
+export const Title = styled.h3``;
+
+export const Icon = styled.img`
+  width: 40px;
+  height: 40px;
+`;
