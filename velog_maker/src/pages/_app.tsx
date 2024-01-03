09acdbceaf42097b431eb5bcb558c83bf14262f4
전해림 <HAERIM060920@gmail.com>
2024-01-03 15:33:07 +0900

    remove : 불필요한 파일 삭제

--- velog_maker/src/pages/_app.tsx
+++ velog_maker/src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import PageTemplate from "../components/PageTemplate";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <PageTemplate>
-        <Component {...pageProps} />
-      </PageTemplate>
+      <Component {...pageProps} />
     </RecoilRoot>
   );
 }
